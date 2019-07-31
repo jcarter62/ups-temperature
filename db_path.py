03fa62c822dd4760890f2477e4c2f403f093842f
jcarter62@gmail.com <jcarter62>
2019-07-31 08:28:37 -0700

    config changes

--- db_path.py
+++ db_path.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+import os
 
 class DbPath:
 
@@ -12,5 +13,18 @@ class DbPath:
             print('Error Access ini file: ' + str(e)  )
 
     def database_path(self):
+        # create folder if does not exist
+        self.__dest_folder_exist(self.filepath)
         return self.filepath
+
+    def __dest_folder_exist(self, name):
+        # remove last item in path, and check if folder exists
+        # if it does not, then check if root exists and then create
+        this_folder = os.path.dirname(name)
+        if this_folder > '':
+            if not os.path.exists(this_folder):
+                self.__dest_folder_exist(this_folder)
+            os.makedirs(this_folder, exist_ok=True)
+        return
+
 
---
+++ run.cmd
@@ -0,0 +1,5 @@
+d:
+cd \apps\ups-temperature
+venv\scripts\activate.bat
+python app.py
+exit
